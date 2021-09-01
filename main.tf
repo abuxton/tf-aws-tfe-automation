14916f0d36f6c23d08503b62cb30c718c3357456
Adam Buxton <buxton.adam@gmail.com>
2021-09-01 13:01:45 +0100

    comment out unreq resource for POC validation

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-resource "tfe_organization" "test" {
-  for_each = data.aws_organizations_organization.org
-  name     = data.aws_organizations_organization.org.id
-  email    = data.aws_organizations_organization.org.master_account_email
-}
+// resource "tfe_organization" "test" {
+//   for_each = data.aws_organizations_organization.org
+//   name     = data.aws_organizations_organization.org.id
+//   email    = data.aws_organizations_organization.org.master_account_email
+// }
 
